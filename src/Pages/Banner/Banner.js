512ee7fb107ba0ece3a6ad04175a60ed54e8f56e
eshanHaque <eshan.haque16@gmail.com>
2022-06-08 23:59:45 +0600

    delploy

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
         <div className="hero min-h-screen bgColor">
             <div className="hero-content flex-col lg:flex-row myContent">
             <div class="h-shape"></div>
-                <div className="image"><img src={img} className="max-w-sm rounded-lg shadow-2xl" /></div>
+                <div className="image"><img src={img} className="max-w-sm rounded-lg shadow-2xl" alt='heroimage'/></div>
                 <div className=''>
                     <h1 className="text-5xl font-bold text-white">Hi, I'm <span className='name'>Md. Yusuf Haque</span></h1>
                     <p className="py-6 text-white"> I'm a full stack Web Developer . I have many more different skills which I added below. I have completed Completed API project and web applications.</p>
